2c506d5066062f01ec444d7b373a355eb2b3ac37
Chadrick <chadrickalvarez@gmail.com>
2025-04-14 14:54:28 -0400

    Lab putting it all together

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+function createLoginTracker(userInfo) {
+  const {
+    password = "password123",
+    username = "user1"
+  } = userInfo;
+
+  let attemptCount = 0;
+
+  const LoginAttempt = (passwordAttempt) => {
+    attemptCount++;
+
+
+    if (passwordAttempt === password) {
+      return "Login successful";
+    } 
+    else if (attemptCount === 1) {
+      return "Attempt 1: Login failed";
+    } else if (attemptCount === 2){
+      return "Attempt 2: Login failed";
+    }
+    else if (attemptCount === 3) {
+      return "Attempt 3: Login failed" 
+    }
+    else {
+      return "Account locked due to too many failed login attempts"
+    }
+  };
+
+  return LoginAttempt;
+}
+
+
+// Example usage:
+
+
+
+
+
+
+
 
 
 
